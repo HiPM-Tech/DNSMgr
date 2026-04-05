473df0608d7b9ddef130c6f6a30229d2f50d931b
Pimeng <pimeng@vip.qq.com>
2026-04-05 20:38:27 +0800

    del: 删除 debug 辅助文件

--- fix_security.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-let file = fs.readFileSync('client/src/pages/system/SecurityTab.tsx', 'utf8');
-file = file.replace(/      \)}\n  \);\n\}/, '  );\n}');
-fs.writeFileSync('client/src/pages/system/SecurityTab.tsx', file);
--- fix_security2.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let file = fs.readFileSync('client/src/pages/system/SecurityTab.tsx', 'utf8');
-file = file.replace(/AlertTriangle, /g, '');
-fs.writeFileSync('client/src/pages/system/SecurityTab.tsx', file);
