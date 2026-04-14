ef1783692781165190cc7a5c1cf93d22f9e972ed
Pimeng <pimeng@vip.qq.com>
2026-04-14 20:28:28 +0800

    types(zxcvbn): 完善类型定义

--- server/src/types/zxcvbn.d.ts
+++ server/src/types/zxcvbn.d.ts
@@ -1,9 +1,17 @@
 declare module 'zxcvbn' {
+  interface ZXCVBNFeedback {
+    warning: string;
+    suggestions: string[];
+  }
+
   interface ZXCVBNResult {
     score: number;
-    feedback: {
-      warning: string;
-      suggestions: string[];
+    feedback: ZXCVBNFeedback;
+    crack_times_display: {
+      offline_slow_hashing_1e4_per_second: string;
+      offline_fast_hashing_1e10_per_second: string;
+      online_no_throttling_10_per_second: string;
+      online_throttling_100_per_hour: string;
     };
   }
   function zxcvbn(password: string, userInputs?: string[]): ZXCVBNResult;
